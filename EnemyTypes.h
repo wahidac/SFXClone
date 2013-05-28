584a63077207d530b962bb46a2041f468ddaea17
Wahid <wahidac@gmail.com>
2013-05-28 02:38:41 -0700

    Added folder w/ blender objects we can load. Added library to load blender objects. All necessary files are located under OBJLoader.

---
+++ EnemyTypes.h
@@ -0,0 +1,14 @@
+//
+//  EnemyTypes.h
+//  Dev
+//
+//  Created by Wahid Chowdhury on 5/28/13.
+//  Copyright (c) 2013 Wahid Chowdhury. All rights reserved.
+//
+
+#ifndef __Dev__EnemyTypes__
+#define __Dev__EnemyTypes__
+
+#include <iostream>
+
+#endif /* defined(__Dev__EnemyTypes__) */
